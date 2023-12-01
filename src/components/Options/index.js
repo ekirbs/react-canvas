b9b67ba83bf7d2905faf8ef6366bae589c9a3577
Eric K <erickirberger@gmail.com>
2023-11-30 21:31:27 -0500

    added react icons for options button and some more language for the toggle.

--- src/components/Options/index.js
+++ src/components/Options/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from "react-icons/fa";
 import './optionsbtn.css';
 
 function Options({ toggleOptions }) {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => setShowDropdown(!showDropdown);
+
   return (
     <>
-      <button id="options" onClick={toggleOptions}>
-        Options
-      </button>
+      <div className="button-container">
+        <div className="options-icon" onClick={toggleDropdown}>
+          {showDropdown ? (
+            <FaTimes className="FaTimes, optionsBtn" onClick={toggleOptions} />
+          ) : (
+            <FaBars className="Fatimes, optionsBtn" onClick={toggleOptions} />
+          )}
+        </div>
+      </div>
     </>
   );
 }
