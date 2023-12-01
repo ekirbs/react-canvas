4613975ee070aefa10d72148ec3b42b07e6c6099
Eric K <erickirberger@gmail.com>
2023-12-01 15:33:33 -0500

    working on setting line width

--- src/components/OptionsPanel/index.js
+++ src/components/OptionsPanel/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { FaInfinity } from "react-icons/fa";
 import './optionsPanel.css';
 
 function OptionsPanel() {
+
+  const setLineWidth = (width) => {
+    const canvas = document.getElementById('draw');
+    const ctx = canvas.getContext('2d');
+    ctx.lineWidth = width;
+  };
+
+  const resetToVarWidth = () => {
+    let direction = true;
+  
+    const canvas = document.getElementById('draw');
+    const ctx = canvas.getContext('2d');
+
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+      direction = !direction;
+    }
+    ctx.lineWidth += direction ? 1 : -1;
+  };
+
+  const handleSmLineWidth = () => {
+    setLineWidth(10);
+  };
+
+  const handleMdLineWidth = () => {
+    setLineWidth(50);
+  };
+
+  const handleLgLineWidth = () => {
+    setLineWidth(100);
+  };
 
   return (
     <>
@@ -10,9 +41,10 @@ function OptionsPanel() {
           <div className="lineWidthContainer">
             <h2 className="lineWidthTitle">Line Width</h2>
             <div className="lineWidthButtons">
-              <button className="lineWidthSm"></button>
-              <button className="lineWidthMd"></button>
-              <button className="lineWidthLg"></button>
+              <button className="lineWidthSm" onClick={handleSmLineWidth}></button>
+              <button className="lineWidthMd" onClick={handleMdLineWidth}></button>
+              <button className="lineWidthLg" onClick={handleLgLineWidth}></button>
+              <FaInfinity className="lineWidthVar" onClick={resetToVarWidth} />
             </div>
           </div>
         </div>
